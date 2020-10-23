a3604279b3bd2ff40a3c92c763a7fc3c40dea584
Diogo Marques <diogomarques@outlook.pt>
2020-10-23 21:35:10 +0100

    Update models, api service, environment variables

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from '../shared/services/api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
 
-  constructor() {}
+  private subArray: Subscription[] = [];
+  
+  constructor(protected apiService: ApiService) {}
 
+  showLink(){
+    console.log(this.apiService.getApiUrl());
+  }
+
+  ngOnDestroy(): void {
+    this.subArray.forEach(sub => sub && !sub.closed && sub.unsubscribe);
+  }
 }
--- src/app/shared/services/api-service.service.ts
+++ src/app/shared/services/api-service.service.ts
@@ -2,18 +2,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ApiResponse } from '../models/api-response';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  readonly url = "http://api.icndb.com/";
-
 constructor(protected httpClient: HttpClient) {}
 
-getJoke(id:number | "random"): Observable<ApiResponse<string>>{
-  return this.httpClient.get<ApiResponse<string>>(`${this.url}/jokes/${id}`);
+private addApiKey(url:string): string{
+  return environment.api_url.concat(url,'&key=',environment.api_key);
+}
+
+getApiUrl():string{
+  return this.addApiKey('current.json?q=Lisbon');
+}
+
+getCurrent(location:string): Observable<ApiResponse<string>>{
+  return this.httpClient.get<ApiResponse<string>>(`${environment.api_url}`);
   }
 
 }

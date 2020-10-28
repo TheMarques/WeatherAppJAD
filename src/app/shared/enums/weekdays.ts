06b00d0fa036b4296194c9bdf3d6ede6bdaa6d66
Diogo Marques <diogomarques@outlook.pt>
2020-10-28 19:27:43 +0000

    Update Enums

---
+++ src/app/shared/enums/weekdays.ts
@@ -0,0 +1,9 @@
+export enum Weekday{
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+}
---
+++ src/app/shared/enums/windDirections.ts
@@ -0,0 +1,18 @@
+export enum WindDirections{
+    'N' = "Norte",
+    'NNE' ="Norte-Nordeste" ,
+    'NE' ="Nordeste",
+    'ENE' ="Leste-Nordeste",
+    'E'="Este",
+    'ESE'="Leste-Sudeste",
+    'SE'="Sudeste" ,
+    'SSE'="Sul-Sudeste" ,
+    'S'="Sul" ,
+    'SSW'="Sul-Sudoeste" ,
+    'SW'="Sudoeste",
+    'WSW'="Oeste-Sudoeste"  ,
+    'W'="Oeste",
+    'WNW'="Oeste-Noroeste",
+    'NW'="Nordoeste",
+    'NNW'="Norte-Noroeste"
+}
